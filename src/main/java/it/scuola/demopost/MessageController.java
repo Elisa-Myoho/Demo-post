de5f7b0316f86b74f66da4d032d33c28f957ed6d
Elisa-Myoho <pireddu.elisa@iisbuonarrotivolta.it>
2026-05-20 18:24:06 +0000

    add delete

--- src/main/java/it/scuola/demopost/MessageController.java
+++ src/main/java/it/scuola/demopost/MessageController.java
@@ -1,49 +1,27 @@
 package it.scuola.demopost;
-
 import java.util.List;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
+private final MessageRepository repo;
+public MessageController(MessageRepository repo) {
+this.repo = repo;
+}
 
-    private final MessageRepository repo;
+@GetMapping
+public List<Message> all() {
+return repo.findAll();
+}
 
-    public MessageController(MessageRepository repo) {
-        this.repo = repo;
-    }
+@PostMapping
+public Message create(@RequestBody Message msg) {
+return repo.save(msg);
+}
 
-    @GetMapping("/get")
-    public List<Message> all() {
-        return repo.findAll();
-    }
-
-    // POST JSON: fetch, curl, Postman
-    @PostMapping
-    public Message create(@RequestBody Message msg) {
-        return repo.save(msg);
-    }
-
-    @PostMapping("/post")
-    public ResponseEntity<Void> createFromForm(@RequestParam String text) {
-        Message msg = new Message();
-        msg.setText(text);
-        repo.save(msg);
-
-        // return new RedirectView("index.html");
-
-        return ResponseEntity
-                .status(HttpStatus.SEE_OTHER)
-                .header(HttpHeaders.LOCATION, "/index.html")
-                .build();
-    }
+@DeleteMapping
+public void delete(@RequestBody Message msg) {
+    List<Message> messagesToDelete = repo.findByText(msg.getText());
+    repo.deleteAll(messagesToDelete);
+}
 }
--- src/main/java/it/scuola/demopost/MessageRepository.java
+++ src/main/java/it/scuola/demopost/MessageRepository.java
@@ -1,6 +1,9 @@
 package it.scuola.demopost;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByText(String text);
 }
